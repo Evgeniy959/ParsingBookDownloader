53df710a9d7ed335f1628f419032c5d0950ca0c5
Pili_zr36 <Pili_zr36@itstepru.onmicrosoft.com>
2022-04-04 14:14:57 +0700

    Add project files.

---
+++ ParsingBookDownloader/BooksParser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingBookDownloader
+{
+    class BooksParser
+    {
+        List<Book> books;
+        string baseUrl = "https://tululu.org/txt.php?id=";
+
+        public List<Book> GetBooks(string site)
+        {
+            books = new List<Book>();
+            string name = "";
+            string url = "";
+            int index = site.IndexOf("href=\"/b");
+            while (index >= 0)
+            {
+                //site = site.Remove(0, index + 6);
+                site = site.Remove(0, index + 8);
+                //index = site.IndexOf("\">");
+                index = site.IndexOf("/");
+                if (index >= 0)
+                {
+                    //url = baseUrl + site.Remove(index);
+                    //url = baseUrl + "/txt.php?id" + "=" + site.Remove(index);
+                    url = null;
+                    //site = site.Remove(0, index + 2);
+                    site = site.Remove(0, index + 3);
+                    name = site.Remove(site.IndexOf("</a>"));
+                    books.Add(new Book() { Name = name, Url = url });
+                    index = site.IndexOf("href=\"/b");
+                }
+            }
+            return books;
+        }
+    }
+}
---
+++ ParsingBookDownloader/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ParsingBookDownloader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        FolderBrowserDialog folder;
+        List<Book> books;
+        public MainWindow()
+        {
+            InitializeComponent();
+            folder = new FolderBrowserDialog();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            LB.Items.Clear();
+            SiteToString siteToString = new SiteToString();
+            string site = siteToString.GetSite("https://tululu.org/search/?q=" + TB.Text);
+            BooksParser parser = new BooksParser();
+            books = parser.GetBooks(site);
+
+            foreach (Book x in books)
+            {
+                LB.Items.Add(x.Name);
+                //LB.Items.Add(x.Url);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            folder.ShowDialog();
+        }
+        string GenerateFilename(string bookName)
+        {
+            string result = "";
+            for (int i = 0; i < bookName.Length; i++)
+            {
+                if (bookName[i] == '?' || bookName[i] == '\"' || bookName[i] == '|'
+                    || bookName[i] == '\\' || bookName[i] == ' ' || bookName[i] == '*'
+                    || bookName[i] == '«'
+                    || bookName[i] == '»' || bookName[i] == '>' || bookName[i] == '<'
+                    || bookName[i] == ':' || bookName[i] == '/' || bookName[i] == '\n')
+                    continue;
+                result += bookName[i];
+            }
+            if (result.Length == 0)
+                result = "book";
+            result += ".txt";
+            return result;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (LB.SelectedIndex < 0)
+                return;
+            if (folder.SelectedPath.Length == 0 || folder.SelectedPath == null)
+            {
+                if (folder.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+                    return;
+            }
+            WebClient client = new WebClient();
+            var uri = new Uri(books[LB.SelectedIndex].Url);
+            //if (new Uri(books[LB.SelectedIndex].Url) != null)
+            if (uri == null)
+            {
+                throw new Exception("Ссылка не существует");
+            }
+            try
+            {
+                /*string path = folder.SelectedPath + "\\"
+                 + GenerateFilename(books[LB.SelectedIndex].Name);*/
+                /*string path = folder.SelectedPath + "\\"
+                     + books[LB.SelectedIndex].Name + ".txt";*/
+                string path = $"{folder.SelectedPath}\\{books[LB.SelectedIndex].Name}.txt";
+
+                //client.DownloadFileAsync(new Uri(books[LB.SelectedIndex].Url), path);
+                client.DownloadFileAsync(uri, path);
+                //  System.Windows.Forms.MessageBox.Show("Скачано!");
+
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+
+}
